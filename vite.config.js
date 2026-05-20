0e708747dbf18a834d8cc0a7c08e9efff9e47e4b
Bushra-chohan <51262835+Bushra-chohan@users.noreply.github.com>
2026-05-20 19:29:06 +0500

    fix: change base path for Vite configuration

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/NatureMart/',
+  base: '/nature-mart-ecommerce/',
   plugins: [react(),
     tailwindcss(),
   ],
